13165a51e83a9b87b3ba3680d526b27c872e51ac
abeer-rozba <abeerrozba3@gmail.com>
2026-04-13 15:39:56 +0300

    edit parkRouter.js - corrected the calling of viewParkById

--- routes/parkRouter.js
+++ routes/parkRouter.js
@@ -3,9 +3,9 @@ const router = express.Router()
 const parkController = require('../controllers/ParkController')
 
 router.get('/', parkController.getAllParks)
-router.get('/new', parkController.showNewParkPage)
+// router.get('/new', parkController.showNewParkPage)
 router.post('/new', parkController.createPark)
-router.get('/:id', parkController.showParkById)
+router.get('/:id', parkController.viewParkById)
 router.delete('/:id', parkController.deletePark)
 
 module.exports = router
